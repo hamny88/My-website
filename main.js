466d426cfe7192ba011c143355379385dcf0e05d
hamny88 <ellie.hahm0502@gmail.com>
2020-12-16 01:41:41 +0900

    change theme

--- main.js
+++ main.js
@@ -3,13 +3,25 @@ const toggle = document.querySelector(".navbar__toggleBtn"),
     navbar__icons = document.querySelector(".navbar__icons")
     ;
 const ACTIVE = "active";
+var i = 0;
+var txt = 'Hello world!';
+var speed = 100;
 function handleClickEvent(event) {
     console.log("toggle clicks")
     navbar__menu.classList.toggle(ACTIVE);
     navbar__icons.classList.toggle(ACTIVE);
 }
+
+function greeting() {
+    var typed = new Typed('#typed', {
+        stringsElement: '#typed-strings',
+        typeSpeed : 35
+      });
+}
+
 function init() {
     toggle.addEventListener("click",handleClickEvent);
+    greeting();
 }
 
 init();
