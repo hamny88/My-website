bee353ee915086e365c2502eb3c31816aad35043
hamny88 <ellie.hahm0502@gmail.com>
2020-12-29 23:39:26 +0900

    change font

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -4,7 +4,6 @@ function typeName() {
         strings: [`Nayoun hahm,`, 'Ellie,', '01101000 10011000 11110000,', '👩‍💻'],
         typeSpeed: 10,
         backSpeed: 10,
-        fadeOut: true,
         loop: true,
         smartBackspace: true
        
